027a1dfb864336e53a15397dfedafad49198e8e0
jana6600 <jananikolic6600@gmail.com>
2021-12-13 05:43:24 -0800

    first server

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-const PORT = 3000
+const PORT = 3001
 
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 
 app.listen(PORT, () => console.log(`listening port ${PORT}`))
 
-app.get('./api/welcome', (res, req) => {
-  res.send('welcome')
+app.get('/api/welcome', (req, res) => {
+  res.send('welcome!')
 })
 
-app.post('./api/data', (res, req) => {
-  res.json({ res: [{ success: 'welcome' }, { fail: 'Ooops' }] })
+app.get('/api/data', (req, res) => {
+  res.json({ data: [{ success: 'welcome' }, { fail: 'Ooops' }] })
 })
