b1c2f633bb754a8eeb7f1fc841c4584b0bc7f1e2
jana6600 <jananikolic6600@gmail.com>
2021-12-14 04:35:56 -0800

    make newBook

---
+++ routes/api.route.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+  var router = require('express').Router()
+
+  // router.get('/welcome', (req, res) => {})
+
+  // router.post('/data', (req, res) => {})
+}
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const { resetWatchers } = require('nodemon/lib/monitor/watch')
+const PORT = 3001
 
 require('dotenv').config()
 
-const PORT = 3001
+app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`listening port ${PORT}`))
 
 mongoose
-  .connect(URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTechnology: true,
   })
   .then(() => console.log('connect to db'))
-  .err(() => console.log(("couldn't connect to db, here is why:" = err)))
-
-const mongoose = require('mongoose')
+  .catch((err) => console.log(("couldn't connect to db, here is why:", err)))
 
 const bookModel = new Schema(
   {
@@ -32,9 +33,7 @@ Book({ title: 'Book 1', description: 'solid principle', author: 'author1' })
   .then((newBook) => console.log(newBook))
   .catch((err) => console.log(err))
 
-app.use(bodyParser.json())
-
-app.listen(PORT, () => console.log(`listening port ${PORT}`))
+Book.find().then((books) => console.log(books))
 
 const tunnelCheck = (req, res, next) => {
   const tunnel = req.body ? req.body.tunnel : null
@@ -49,22 +48,41 @@ app.post('/', tunnelCheck, (req, res) => {
   res.send('Congratulation!')
 })
 
+app.post('/new-book', (req, res) => {
+  new Book({
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+  })
+    .save()
+    .then((newBook) => {
+      console.log(newBook)
+      res.json({ code: 'ok', message: 'book has been added', book: newBook })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ code: '500', message: 'something went wrong' })
+    })
+})
+
+// require('./routes')(app)
+
 // app.get('/api/welcome', (req, res) => {
 //   res.send('welcome!')
 // })
 
-app.get('/api/data', tunnelCheck, (req, res) => {
-  let someId = req.query.id
-  temp = [
-    {
-      id: 1,
-      age: 3,
-    },
-    {
-      id: 2,
-      age: 13,
-    },
-  ]
-  const result = temp.filter((item) => item.id == someId)
-  res.json(result)
-})
+// app.get('/api/data', tunnelCheck, (req, res) => {
+//   let someId = req.query.id
+//   temp = [
+//     {
+//       id: 1,
+//       age: 3,
+//     },
+//     {
+//       id: 2,
+//       age: 13,
+//     },
+//   ]
+//   const result = temp.filter((item) => item.id == someId)
+//   res.json(result)
+// })
