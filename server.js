11d855deaa16182673f116edc13c09b4da22c96e
jana6600 <jananikolic6600@gmail.com>
2021-12-15 02:00:42 -0800

    crud author

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const res = require('express/lib/response')
+const { required } = require('nodemon/lib/config')
 const PORT = 3001
 
 require('dotenv').config()
@@ -18,9 +18,9 @@ mongoose
 
 const bookModel = mongoose.Schema(
   {
-    title: { type: String, require: true },
+    title: { type: String, required: true },
     description: { type: String },
-    author: { type: String, require: true },
+    author: { type: String, required: true },
   },
   { timestamps: true },
 )
@@ -44,7 +44,7 @@ app.post('/api/new-book', (req, res) => {
     })
 })
 
-app.get('api/get-books', (req, res) => {
+app.get('/api/get-books', (req, res) => {
   // let search = Book.find({
   //   title: new RegExp(`${req.query.title}`, 'ig'),
   //   description: new RegExp(`${req.query.description}`, 'ig'),
@@ -54,12 +54,60 @@ app.get('api/get-books', (req, res) => {
   // let search = req.query.title
 
   const query = req.query.title
-    ? { title: new RegExp(`${req.query.title}, 'ig`) }
+    ? { title: new RegExp(`${req.query.title}`, 'ig') }
     : {}
 
   Book.find(query).then((books) => {
     res.json(books)
   })
+})
+
+const authorModel = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    genre: { type: String, required: true },
+    number: { type: String, required: true },
+  },
+  { timestamps: true },
+)
+
+const Author = mongoose.model('Author', authorModel)
+
+app.post('/api/add-author', (req, res) => {
+  new Author({
+    name: req.body.name,
+    address: req.body.address,
+    genre: req.body.genre,
+    number: req.body.number,
+  })
+    .save()
+    .then((newAuthor) => {
+      res.json({ message: 'add new author' })
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'something went wrong' })
+    })
+})
+
+app.put('/api/update-author/:id', (req, res) => {
+  Author.updateOne({ id: req.query.id })
+    .then(() => {
+      res.status(201).json({ message: 'author updated successfully' })
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'something went wrong' })
+    })
+})
+
+app.delete('/api/delete-author/:id', (req, res) => {
+  Author.deleteOne({ id: req.query.id })
+    .then(() => {
+      res.json({ message: 'author deleted successfully' })
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'something went wrong' })
+    })
 })
 
 app.listen(PORT, () => console.log(`listening port ${PORT}`))
